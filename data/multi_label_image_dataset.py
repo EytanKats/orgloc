0426816b0af74b4899e8cc5d773f99f883ff2432
EytanKats <eytan.kats@gmail.com>
2025-03-17 21:27:39 +0100

    remove deprecated parameter

--- data/multi_label_image_dataset.py
+++ data/multi_label_image_dataset.py
@@ -53,17 +53,17 @@ class Image_Dataset(Dataset):
     def get_transforms(self):
         if self.stage == 'train':
             transforms = A.Compose([
-                A.ToFloat(max_value=255.0, always_apply=True),
+                A.ToFloat(max_value=255.0),
                 # A.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.0, p=0.5),
                 A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.0, rotate_limit=15, border_mode=cv2.BORDER_CONSTANT, p=0.5),
-                A.Resize(self.img_size, self.img_size, always_apply=True),
-                ToTensorV2(always_apply=True),
+                A.Resize(self.img_size, self.img_size),
+                ToTensorV2(),
             ])
         else:
             transforms = A.Compose([
-                A.ToFloat(max_value=255.0, always_apply=True),
-                A.Resize(self.img_size, self.img_size, always_apply=True),
-                ToTensorV2(always_apply=True),
+                A.ToFloat(max_value=255.0),
+                A.Resize(self.img_size, self.img_size),
+                ToTensorV2(),
             ])
         return transforms
 
